9de794227cf8b6fd499e9b82653d72a238ace7f2
miroshnikoff <taxi83080@gmail.com>
2023-10-28 01:00:16 +0300

    add payment model

--- courses/models.py
+++ courses/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from users.models import NULLABLE
+from users.models import NULLABLE, User
 
 
 class Course(models.Model):
@@ -22,7 +22,8 @@ class Lesson(models.Model):
     preview = models.ImageField(upload_to='courses/', verbose_name='Превью', **NULLABLE)
     description = models.TextField(verbose_name='Описание', **NULLABLE)
     video_url = models.URLField(verbose_name='Видео', **NULLABLE)
-    course = models.ForeignKey(Course, on_delete=models.SET_NULL, **NULLABLE, verbose_name='Курс')
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, **NULLABLE, related_name='lesson',
+                               verbose_name='Курс')
 
     def __str__(self):
         return self.title
@@ -31,3 +32,15 @@ class Lesson(models.Model):
         verbose_name = 'Урок'
         verbose_name_plural = 'Уроки'
         ordering = ('pk',)
+
+
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, **NULLABLE, related_name='payment',
+                             verbose_name='Пользователь')
+    date = models.DateTimeField(auto_now_add=True, **NULLABLE, verbose_name='Дата оплаты')
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, **NULLABLE, related_name='payment',
+                               verbose_name='Оплаченный курс')
+    lesson = models.ForeignKey(Lesson, on_delete=models.SET_NULL, **NULLABLE, related_name='payment',
+                               verbose_name='Оплаченный урок')
+    total_paid = models.IntegerField(verbose_name='Сумма оплаты')
+    method = models.CharField(max_length=100, **NULLABLE, verbose_name='Способ оплаты')
