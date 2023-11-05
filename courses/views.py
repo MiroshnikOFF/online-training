a65e8cd24bdc668a20591b95f13fe8fa4a545dd5
miroshnikoff <taxi83080@gmail.com>
2023-11-05 23:53:33 +0300

    refactor course views

--- courses/views.py
+++ courses/views.py
@@ -1,16 +1,19 @@
 from rest_framework import viewsets, generics
 from rest_framework.filters import OrderingFilter
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from django_filters.rest_framework import DjangoFilterBackend
 
-from courses.models import Course, Lesson, Payment
+from courses.models import Course, Lesson, Payment, Subscription
+from courses.pagination import CoursesLessonsPaginator
 from courses.permissions import IsNotModeratorForViewSet, IsOwner, IsNotModeratorForAPIView
-from courses.serializers import CourseSerializer, LessonSerializer, PaymentSerializer
+from courses.serializers import CourseSerializer, LessonSerializer, PaymentSerializer, SubscriptionSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated, IsNotModeratorForViewSet,)
     serializer_class = CourseSerializer
+    # queryset = Course.objects.all()
+    pagination_class = CoursesLessonsPaginator
 
     def perform_create(self, serializer):
         """Сохраняет авторизованного пользователя в объекте курса"""
@@ -43,6 +46,7 @@ class LessonCreateAPIView(generics.CreateAPIView):
 class LessonListAPIView(generics.ListAPIView):
     permission_classes = (IsAuthenticated,)
     serializer_class = LessonSerializer
+    pagination_class = CoursesLessonsPaginator
 
     def get_queryset(self):
         """Получает уроки авторизованного пользователя или все уроки для персонала"""
@@ -78,3 +82,38 @@ class PaymentListAPIView(generics.ListAPIView):
     ordering_fields = ('date',)
     filterset_fields = ('course', 'lesson', 'method',)
 
+
+class SubscriptionCreateAPIView(generics.CreateAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = SubscriptionSerializer
+
+    def perform_create(self, serializer):
+        """Сохраняет авторизованного пользователя в объекте подписки"""
+
+        new_subscription = serializer.save()
+        new_subscription.user = self.request.user
+        new_subscription.subscribed = True
+        new_subscription.save()
+
+
+class SubscriptionListAPIView(generics.ListAPIView):
+    permission_classes = (IsAuthenticated, IsAdminUser,)
+    serializer_class = SubscriptionSerializer
+    queryset = Subscription.objects.all()
+
+
+class SubscriptionRetrieveAPIView(generics.RetrieveAPIView):
+    permission_classes = (IsAuthenticated, IsAdminUser,)
+    serializer_class = SubscriptionSerializer
+    queryset = Subscription.objects.all()
+
+
+class SubscriptionUpdateAPIView(generics.UpdateAPIView):
+    permission_classes = (IsAuthenticated, IsAdminUser,)
+    serializer_class = SubscriptionSerializer
+    queryset = Subscription.objects.all()
+
+
+class SubscriptionDestroyAPIView(generics.DestroyAPIView):
+    permission_classes = (IsAuthenticated,)
+    queryset = Subscription.objects.all()
